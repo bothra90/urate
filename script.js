0a7adc2025a960a640bbc20bedae24856ad39be4
nikhilsimha <r.nikhilsimha@gmail.com>
2012-01-29 05:28:18 +0530

    NS: part script ready

--- script.js
+++ script.js
@@ -1,8 +1,27 @@
+
+function klas(data){ console.log(data);}
+
+
+
+
+
+
+
+
 var children=document.getElementById("watch-related").children;
 var len = children["length"];
-for (i= 0; i<len; i++)
-{
+for (i= 0; i<len; i++){
  var suggestion = children[i];
  if (suggestion.getAttribute("class") === "video-list-item ")
-	console.log(suggestion.children[0].getAttribute("href").slice(9,20));
+	//console.log(suggestion.children[0].getAttribute("href").slice(9,20));
+	var v_id=suggestion.children[0].getAttribute("href").slice(9,20);
+	console.log(v_id);
+	var str='https://gdata.youtube.com/feeds/api/videos/'+v_id+'?v=2&alt=json&callback=klas';
+	
+	var script_= document.createElement("script");
+	script_.type="text/javascript";
+	script_.src=str;
+	document.body.appendChild(script_);
+	
+	
 }
