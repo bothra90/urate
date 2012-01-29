850bf81306e6bd30c2ed7f39da4e3fa91b0e74aa
Abhishek Asawa <abhiasawa@gmail.com>
2012-01-29 09:18:23 +0530

    AA: Changed the way communication occures. Added Background page which receives requests and responds to the script page

--- script.js
+++ script.js
@@ -1,5 +1,4 @@
 
-function klas(data){ console.log(data);}
 
 var children=document.getElementById("watch-related").children;
 var len = children["length"];
@@ -7,13 +6,18 @@ for (i= 0; i<len; i++){
  var suggestion = children[i];
  if (suggestion.getAttribute("class") === "video-list-item ")
 	//console.log(suggestion.children[0].getAttribute("href").slice(9,20));
+	
 	var v_id=suggestion.children[0].getAttribute("href").slice(9,20);
 	console.log(v_id);
-	var str='https://gdata.youtube.com/feeds/api/videos/'+v_id+'?v=2&alt=json&callback=kals';
 	
-	var script_= document.createElement("JSOn_Script");
+	chrome.extension.sendRequest({video_id : v_id},function(res){console.log(res.w_id)});
+	
+	/*
+	var str="http://gdata.youtube.com/feeds/api/videos/"+v_id+"?v=2&amp;alt=json-in-script&amp;callback=klas";
+	//var str = "http://gdata.youtube.com/feeds/users/GoogleDevelopers/uploads?v=2&alt=json-in-script&format=5&callback=klas"
+	var script_= background.createElement("JSOn_Script");
 	script_.type="text/javascript";
 	script_.src=str;
-	document.body.appendChild(script_);
-	
+	background.body.appendChild(script_);
+	*/
 }
