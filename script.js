cf2681b68aa4769bcb35831934827bab4c202396
Abhay <bothra90@gmail.com>
2012-01-29 12:03:50 +0530

    AKB: Prototype working...yay

--- script.js
+++ script.js
@@ -2,31 +2,32 @@ var children=document.getElementById("watch-related").children;
 var len = children["length"];
 
 for (i= 0; i<len; i++){
- var suggestion = children[i];
- if (suggestion.getAttribute("class") === "video-list-item ")	
-	var v_id=suggestion.children[0].getAttribute("href").slice(9,20);
-	console.log(v_id);
-	
-	var str='https://gdata.youtube.com/feeds/api/videos/'+v_id+'?v=2';	
-	var xmlhttp=new XMLHttpRequest();
-	xmlhttp.open("GET",str,true);
-	
-	xmlhttp.onreadystatechange=function(){
-	console.log(xmlhttp.readyState);
-		if(xmlhttp.readyState==4 ){
-			console.log("blah");
-			var xmlDoc=xmlhttp.responseXML;
-			//var x=xmlDoc.getElementsByTagName("yt:rating");
-			console.log(xmlDoc);
-			console.log(x.length);
-			
-			for(i=0;i<x.length;i++){
-				console.log(x);
-			}
-		}		
-	}
-	console.log(xmlhttp.readyState);
-	
-	xmlhttp.send();
-	
+  var suggestion = children[i];
+  if (suggestion.getAttribute("class") === "video-list-item ")	
+  {
+    var v_id=suggestion.children[0].getAttribute("href").slice(9,20);
+    console.log(v_id);
+    var str='https://gdata.youtube.com/feeds/api/videos/'+v_id+'?v=2';	
+    var xmlhttp=new XMLHttpRequest();
+    xmlhttp.open("GET",str,false);
+    console.log(str);
+    xmlhttp.onreadystatechange = function(){
+      console.log(xmlhttp.readyState);
+      if(xmlhttp.readyState === 4 ){
+	var xmlDoc=xmlhttp.responseXML;
+	var x = xmlDoc.getElementsByTagNameNS("http://gdata.youtube.com/schemas/2007","rating")[0];
+	  console.log(x);
+	  console.log(xmlDoc);
+	  var likes = x.getAttribute("numDislikes");
+	  var dislikes = x.getAttribute("numLikes");
+	  console.log(likes);
+	  console.log(dislikes);
+	  console.log(xmlhttp.readyState);
+	  var info = document.createElement("span");
+	  info.innerHTML = "Likes: "+ likes + ", Dislikes: " + dislikes;
+	  suggestion.children[0].appendChild(info);
+      }		
+    };
+    xmlhttp.send();
+  }
 }
