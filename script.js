1d6f0ec473957a22c0af6c8d11cf2094c7577a4c
Abhay <bothra90@gmail.com>
2012-01-31 02:00:03 +0530

    some problems in using page action instead of browser action. Needs to be fixed

--- script.js
+++ script.js
@@ -1,6 +1,5 @@
 var children=document.getElementById("watch-related").children;
 var len = children["length"];
-var requests = new Array()
 for (var i= 0; i<len; i++){
   var suggestion = children[i];
   if (suggestion.getAttribute("class") === "video-list-item ")	
@@ -8,13 +7,10 @@ for (var i= 0; i<len; i++){
     var v_id=suggestion.children[0].getAttribute("href").slice(9,20);
     console.log(v_id);
     var str='https://gdata.youtube.com/feeds/api/videos/'+v_id+'?v=2';	
-    makeRequest(str);
-    requests[v_id] = new XMLHttpRequest();
-    var xmlhttp = requests[v_id]
-    xmlhttp.open("GET",str,true);
+    var xmlhttp = new XMLHttpRequest();
+    xmlhttp.open("GET",str,false);
     console.log(str);
     xmlhttp.onreadystatechange = function(){
-      xmlhttp = requests[v_id];
       console.log(xmlhttp.readyState);
       if(xmlhttp.readyState === 4 ){
 	var xmlDoc=xmlhttp.responseXML;
@@ -32,6 +28,5 @@ for (var i= 0; i<len; i++){
       }		
     };
     xmlhttp.send();
-
   }
 }
